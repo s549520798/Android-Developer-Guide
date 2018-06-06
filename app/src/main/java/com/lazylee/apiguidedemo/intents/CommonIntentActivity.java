dff740e691b9144088d8c01bccfb8a77a62295af
lzy <lazylee7777@163.com>
2018-06-06 22:21:48 +0800

    change

--- app/src/main/java/com/lazylee/apiguidedemo/intents/CommonIntentActivity.java
+++ app/src/main/java/com/lazylee/apiguidedemo/intents/CommonIntentActivity.java
@@ -8,9 +8,11 @@ import android.provider.AlarmClock;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,91 +23,47 @@ import com.lazylee.apiguidedemo.R;
 public class CommonIntentActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "CommonIntentActivity";
     public static final int MY_PERMISSIONS_REQUEST_RET_ALARM = 1;
+    private Toolbar toolbar;
     private Button btnSetAlarm;
+    private Button btnSetTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.common_intent_activity);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(TAG);
+        }
         btnSetAlarm = findViewById(R.id.button_alarm);
+        btnSetTimer = findViewById(R.id.button_teimer);
+        findViewById(R.id.button_show_alarm).setOnClickListener(this);
         btnSetAlarm.setOnClickListener(this);
+        btnSetTimer.setOnClickListener(this);
+        findViewById(R.id.button_insert).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button_alarm:
-
-                if (ContextCompat.checkSelfPermission(this, Manifest.permission.SET_ALARM)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    //请求权限
-                    // Should we show an explanation?
-                    Log.d(TAG, "onClick: 没有权限，开始请求权限");
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.SET_ALARM)) {
-                        AlertDialog builder = new AlertDialog.Builder(this)
-                                .setTitle("权限提示")
-                                .setMessage("需要允许权限，才能使用设置闹钟功能")
-                                .setPositiveButton("授权", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        ActivityCompat.requestPermissions(CommonIntentActivity.this,
-                                                new String[]{Manifest.permission.SET_ALARM},
-                                                MY_PERMISSIONS_REQUEST_RET_ALARM);
-                                    }
-                                }).setNegativeButton("取消", null)
-                                .create();
-                        builder.show();
-                        // Show an expanation to the user *asynchronously* -- don't block
-                        // this thread waiting for the user's response! After the user
-                        // sees the explanation, try again to request the permission.
-
-                    } else {
-
-                        // No explanation needed, we can request the permission.
-                        Log.d(TAG, "onClick: 没有提示用处就开始请求权限");
-                        ActivityCompat.requestPermissions(this,
-                                new String[]{Manifest.permission.SET_ALARM},
-                                MY_PERMISSIONS_REQUEST_RET_ALARM);
-
-                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                        // app-defined int constant. The callback method gets the
-                        // result of the request.
-                    }
-                } else {
-                    Log.d(TAG, "onClick: 已经有权限，设置闹钟");
-                    createAlarm();
-                }
-
-
+                createAlarm();
+                break;
+            case R.id.button_teimer:
+                setTimer("测试Intent",200);
+                break;
+            case R.id.button_show_alarm:
+                showAlarms();
+                break;
+            case R.id.button_insert:
                 break;
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_RET_ALARM:
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "onRequestPermissionsResult: 获取到了权限");
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    createAlarm();
 
-                } else {
-                    Log.d(TAG, "onRequestPermissionsResult: 没有获取到权限");
-                    createAlarm();
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                break;
-            default:
-                break;
-        }
-    }
 
     private void createAlarm() {
         Intent intent = new Intent();
@@ -113,11 +71,30 @@ public class CommonIntentActivity extends AppCompatActivity implements View.OnCl
         intent.putExtra(AlarmClock.EXTRA_MESSAGE, "测试用闹钟");
         intent.putExtra(AlarmClock.EXTRA_HOUR, 5);
         intent.putExtra(AlarmClock.EXTRA_MINUTES, 30);
-        intent.putExtra(AlarmClock.EXTRA_SKIP_UI,true);
+        intent.putExtra(AlarmClock.EXTRA_SKIP_UI,false);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         } else {
             Toast.makeText(this, "没有设置闹钟的应用", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void setTimer(String message, int seconds){
+        Intent intent = new Intent(AlarmClock.ACTION_SET_TIMER)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_LENGTH, seconds)
+                .putExtra(AlarmClock.EXTRA_SKIP_UI, false);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+    private void showAlarms(){
+        Intent intent = new Intent(AlarmClock.ACTION_SHOW_ALARMS);
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
+    private void insertDate(){
+
     }
 }
