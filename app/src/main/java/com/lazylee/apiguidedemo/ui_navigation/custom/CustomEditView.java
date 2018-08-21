47fd384d643aa9394aa64d7c86cb5e7e6d32c20e
lzy <lazylee7777@163.com>
2018-08-22 00:10:32 +0800

    dial view

--- app/src/main/java/com/lazylee/apiguidedemo/ui_navigation/custom/CustomEditView.java
+++ app/src/main/java/com/lazylee/apiguidedemo/ui_navigation/custom/CustomEditView.java
@@ -1,15 +1,13 @@
-package com.lazylee.apiguidedemo.ui_navigation.customedit;
+package com.lazylee.apiguidedemo.ui_navigation.custom;
 
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.content.res.AppCompatResources;
 import android.support.v7.widget.AppCompatEditText;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.util.LayoutDirection;
 import android.view.MotionEvent;
 import android.view.View;
 
---
+++ app/src/main/java/com/lazylee/apiguidedemo/ui_navigation/custom/DialView.java
@@ -0,0 +1,79 @@
+package com.lazylee.apiguidedemo.ui_navigation.custom;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+import android.view.View;
+
+public class DialView extends View {
+
+    private static final int SELECTION_COUNT = 4;
+    private Paint mTextPaint;    //paint for draw text
+    private Paint mDialPaint;    //paint for draw circle background
+
+    private float mWidth;   //宽
+    private float mHeight;  //高
+    private float mRadius;  //半径
+
+    private int mActiveSelection; //当前选择的挡位
+    // String buffer for dial labels and float for ComputeXY result.
+    private final StringBuffer mTempLabel = new StringBuffer(8);
+    private final float[] mTempResult = new float[2];
+
+    public DialView(Context context) {
+        super(context);
+        init();
+    }
+
+    public DialView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public DialView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+
+    private void init() {
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setColor(Color.BLACK);
+        mTextPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+        mTextPaint.setTextSize(40f);
+        mDialPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mDialPaint.setColor(Color.GRAY);
+        // Initialize current selection.
+        mActiveSelection = 0;
+        // TODO: Set up onClick listener for this view.
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        // Calculate the radius from the width and height.
+        mWidth = w;
+        mHeight = h;
+        mRadius = (float) (Math.min(mWidth, mHeight) / 2 * 0.8);
+    }
+
+    private float[] computeXYForPosition
+            (final int pos, final float radius) {
+        float[] result = mTempResult;
+        Double startAngle = Math.PI * (9 / 8d);   // Angles are in radians.
+        Double angle = startAngle + (pos * (Math.PI / 4));
+        result[0] = (float) (radius * Math.cos(angle)) + (mWidth / 2);
+        result[1] = (float) (radius * Math.sin(angle)) + (mHeight / 2);
+        return result;
+    }
+}
